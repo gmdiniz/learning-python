b1bf830c39b3dbbc7c2a74fb572daf7e64974c3e
gmdiniz <gabrieldinizmota@hotmail.com>
2022-12-17 22:52:58 -0300

    Aula 3 - Strings

---
+++ aula3.py
@@ -0,0 +1,14 @@
+'''
+Python = LP 
+Fortemente tipado e dinâmico
+'''
+
+print('Hello World')
+
+print("Hello World")
+
+print('Hello "World"')
+
+print("Hello \"World\"")
+
+print(r'Hello \"World\"')
