90d73e3e66c5099e22ba48d60eb2f6d5a06535b3
gmdiniz <gabrieldinizmota@hotmail.com>
2022-12-20 22:39:07 -0300

    Math operators

---
+++ basics_of_python/aua10.py
@@ -0,0 +1,7 @@
+concat = 'A' + 'B' + 'C'
+print(concat)
+
+a_dez_vezes = 'A' * 10
+gabriel_3_vezes = 3 * 'Gabriel'
+print(a_dez_vezes)
+print(gabriel_3_vezes)
---
+++ basics_of_python/aula9.py
@@ -0,0 +1,20 @@
+soma = 10 + 10
+print(soma)
+
+subtracao = 15 - 10
+print(subtracao)
+
+multiplicacao = 10 * 10
+print(multiplicacao)
+
+divisao = 10 / 2 # sempre retorna float
+print(divisao)
+
+divisao_inteira = 10 // 2.2  
+print(divisao_inteira)
+
+exponenciacao = 2 ** 2
+print(exponenciacao)
+
+modulo = 55 % 2
+print(modulo)
