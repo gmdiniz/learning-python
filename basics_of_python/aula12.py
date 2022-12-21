91a68e992f62d6350bc41f038baf4c797c29b48f
gmdiniz <gabrieldinizmota@hotmail.com>
2022-12-20 23:03:03 -0300

    Practice

---
+++ basics_of_python/aula12.py
@@ -0,0 +1,11 @@
+nome = 'Gabriel Diniz Mota'
+altura = 1.80
+peso = 85
+imc = peso / (altura ** 2)
+print(imc)
+
+print(nome, 'tem', altura, 'de altura')
+print('pesa', peso, 'quilos e seu imc é')
+print(imc)
+
+print(...) # elipsis
